6138f8232c19424beb17becb2eace6e6c5b62e2e
Dilbeer SIngh <dilbeer0official@gmail.com>
2022-08-19 19:29:59 +0530

    New

--- src/components/background/card-background/Cards/cardBack/back.js
+++ src/components/background/card-background/Cards/cardBack/back.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./styles.css";
 function Back() {
-  return <div className="back-section">Card back</div>;
+  return (
+    <div className="backCard">
+      <img src="images/bg-card-back.png" alt="front" />
+    </div>
+  );
 }
 
 export { Back };
--- src/components/background/card-background/Cards/cardFront/front.js
+++ src/components/background/card-background/Cards/cardFront/front.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./styles.css";
 function Front() {
-  return <div className="front-section">Card Front</div>;
+  return (
+    <div className="frontCard">
+      <img src="images/bg-card-front.png" alt="front" />
+    </div>
+  );
 }
 
 export { Front };
