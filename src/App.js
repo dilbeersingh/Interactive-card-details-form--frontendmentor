fcd6e084312ae9f9e833c59ab85982c3c0369839
Dilbeer SIngh <dilbeer0official@gmail.com>
2022-08-19 12:11:41 +0530

    Sandbox Connection

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ export default function App() {
     <div className="App">
       <div class="attribution">
         Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
+        . Coded by{" "}
+        <a href="." rel="noreferrer" target="_blank">
+          Your Name Here
+        </a>
+        .
       </div>
     </div>
   );
